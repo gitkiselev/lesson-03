b41378bdf2403da516303ade9680b270315208f8
gitkiselev <482003@mail.ru>
2019-11-05 17:18:39 +0300

    hw04 basic done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,7 +6,22 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 import './app.css';
 
+
 const App = () => {
+    
+    
+    let data = [
+        1,
+        undefined,
+        NaN,
+        ['dfdf', 1],
+        null,
+        {label: 'Going to learn React', important: true, id: 'qwefg'},
+        {label: 'That is so good', important: false, id: 'ghnbg'},
+        {label: 'I need a break...', important: false, id: 'hjhgf'}
+    ];
+    
+    data = data.filter(item => typeof item === "object" && !Array.isArray(item) && item !== null);
     return (
         <div className="app">
             <AppHeader />
@@ -14,7 +29,7 @@ const App = () => {
                 <SearchPanel />
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     ) 
